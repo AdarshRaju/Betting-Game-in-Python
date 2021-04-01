cf78d4a7a83e6de8016b0f396a9770876e833592
AdarshRaju <66949659+AdarshRaju@users.noreply.github.com>
2021-04-02 00:26:38 +1100

    Update Betting-Game.py

--- Betting-Game.py
+++ Betting-Game.py
@@ -1,48 +1,31 @@
-print('Project - Math Tutor')
+print('Betting Game')
 
-import random, string, timeit
+import random
 
-x = 12
-def tutor():
-    print(x)
-    no_of_questions = int(input("Enter no. of questions:"))
-    highest_digit = int(input("Enter how many digits you need:"))
+Money = 1000;
+Rounds = int(input("Enter no. of rounds:"))
+
+Marbles ="GGGGGBRRRW"
+
+
+Bet_final = 0;
+for i in range(Rounds):
     
-    score = 0
-    array1 = []
-    array2 = []
-    for i in range(no_of_questions):
-        numa = (random.randrange(0, 10**highest_digit))
-        numb = (random.randrange(0, 10**highest_digit))
-        oper = random.choice(['+', '-', '*', '/'])
+    Bet_amount = int(input("Enter your bet:"))
+    Selected = random.choice(Marbles)
 
-        if oper == '+':
-            correct = int(numa) + int(numb)
-        if oper == '-':
-            correct = int(numa) - int(numb)
-        if oper == '*':
-            correct = int(numa) * int(numb)
-        if oper == '/':
-            numb = random.randrange(1, 10**highest_digit)
-            correct = int(numa) / int(numb)
-                    
-            
-        question = str(numa) + oper + str(numb)
-    
-        answer = input(f'{question}?')
-       
-        if int(answer) == correct:
-            score += 1
-        print(numa, numb)
-        array1.append(question)
-        array2.append(answer)
-        table = dict(zip(array1, array2))
-    
-    print(f'Your score is {score}/{no_of_questions}')
-    print(f'Percentage correct is {score*100/no_of_questions}')
-    print(f'The record of questions and answers are: {table}')
-    print('Thanks for playing!')
-    return (1)
-  
-print(f'The total time is {timeit.timeit(\'tutor()\', globals=globals(), number =1)}')        
+    if Selected == "G":
+        Bet_final = Bet_amount
+    elif Selected == "R":
+        Bet_final = -Bet_amount
+    elif Selected == "B":
+        Bet_final = 10*Bet_amount
+    elif Selected == "W":
+        Bet_final = -5*Bet_amount
 
+    Money = Money + Bet_final
+    print(Money)
+    
+    if Money <= 500:
+        print("Game Over")
+        break
